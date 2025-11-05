6af349b5152b7e507ab92d74aab70d6a4a0ec654
Souleymane DIALLO <sdley2020@gmail.com>
2025-11-05 16:41:29 +0000

    feat(ui): swap equals and power buttons in calculator layout

--- api/controller.js
+++ api/controller.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
-exports.calculate = function(req, res) {
-  req.app.use(function(err, _req, res, next) {
+exports.calculate = function (req, res) {
+  req.app.use(function (err, _req, res, next) {
     if (res.headersSent) {
       return next(err);
     }
@@ -11,10 +11,21 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a, b) { return Number(a) + Number(b) },
-    'subtract': function(a, b) { return a - b },
-    'multiply': function(a, b) { return a * b },
-    'divide':   function(a, b) { return a / b },
+    add: function (a, b) {
+      return Number(a) + Number(b);
+    },
+    subtract: function (a, b) {
+      return a - b;
+    },
+    multiply: function (a, b) {
+      return a * b;
+    },
+    divide: function (a, b) {
+      return a / b;
+    },
+    power: function (a, b) {
+      return Math.pow(a, b);
+    },
   };
 
   if (!req.query.operation) {
@@ -27,15 +38,19 @@ exports.calculate = function(req, res) {
     throw new Error("Invalid operation: " + req.query.operation);
   }
 
-  if (!req.query.operand1 ||
-      !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand1 ||
+    !req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand1.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand1: " + req.query.operand1);
   }
 
-  if (!req.query.operand2 ||
-      !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
-      req.query.operand2.replace(/[-0-9e]/g, '').length > 1) {
+  if (
+    !req.query.operand2 ||
+    !req.query.operand2.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
+    req.query.operand2.replace(/[-0-9e]/g, "").length > 1
+  ) {
     throw new Error("Invalid operand2: " + req.query.operand2);
   }
 
